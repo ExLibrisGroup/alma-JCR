9cf12c3a5b08d7eb5b3f52dc6f13f571cbf11091
avichai-exlibris <avichai.babo@exlibrisgroup.com>
2023-06-06 09:47:21 +0300

    Fix URM-196244
    Incite integration doesn't work in PROD environments

--- cloudapp/src/app/service/InCites.api.service.ts
+++ cloudapp/src/app/service/InCites.api.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { CloudAppEventsService, InitData } from '@exlibris/exl-cloudapp-angular-lib';
 import { mergeMap } from 'rxjs/operators';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { BaseService } from './base.service';
 @Injectable({
   providedIn: 'root'
@@ -10,6 +10,7 @@ import { BaseService } from './base.service';
 
 export class InCitesApiService extends BaseService {
     private _toConnect : boolean;
+    private _authorization : any;
 
     constructor(
         protected eventsService: CloudAppEventsService,
@@ -26,11 +27,11 @@ export class InCitesApiService extends BaseService {
             mergeMap(initData => {
                 console.log("init data = " + initData);
                 fullUrl = this.setBaseUrl(initData);
-
                 return this.getAuthToken()
             }),
             mergeMap(authToken => {
                 let headers = this.setAuthHeader(authToken);
+                this._authorization = headers.Authorization;
                 return this.http.post<any>(fullUrl, { headers })
             })
         );
@@ -45,6 +46,9 @@ export class InCitesApiService extends BaseService {
                 return this.getAuthToken()
             }),
             mergeMap(authToken => {
+                let headers = this.setAuthHeader(authToken);
+                this._authorization = headers.Authorization;
+              
                 return this.http.get<any>(fullUrl, {  })
             })
         );
@@ -59,11 +63,13 @@ export class InCitesApiService extends BaseService {
 
     private getQueryParams() :string{
         let urlParams = "";
-        urlParams = urlParams + QueryParams.Optin + "=" + this._toConnect;
+        urlParams = urlParams + QueryParams.Optin + "=" + this._toConnect +"&" + QueryParams.Jwt + "=" + this._authorization;
         return urlParams;
     }
+    
 }
 
 export enum QueryParams {
-    Optin = "optin"
+    Optin = "optin",
+    Jwt = "jwt"
   }

da63a89220484f4069f3327ef419ea3eda181da0
avichai-exlibris <avichai.babo@exlibrisgroup.com>
2023-06-07 12:18:18 +0300

    Fix URM-196244
    Incite integration doesn't work in PROD environments
    Send the query params for http post only

--- cloudapp/src/app/service/InCites.api.service.ts
+++ cloudapp/src/app/service/InCites.api.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { CloudAppEventsService, InitData } from '@exlibris/exl-cloudapp-angular-lib';
 import { mergeMap } from 'rxjs/operators';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { BaseService } from './base.service';
 @Injectable({
   providedIn: 'root'
@@ -10,6 +10,7 @@ import { BaseService } from './base.service';
 
 export class InCitesApiService extends BaseService {
     private _toConnect : boolean;
+    private _isQueryParamsNeeded : boolean = false;
     private _authorization : any;
 
     constructor(
@@ -26,6 +27,7 @@ export class InCitesApiService extends BaseService {
         return this.getInitData().pipe(
             mergeMap(initData => {
                 console.log("init data = " + initData);
+                this._isQueryParamsNeeded = true;
                 fullUrl = this.setBaseUrl(initData);
                 return this.getAuthToken()
             }),
@@ -42,13 +44,13 @@ export class InCitesApiService extends BaseService {
         return this.getInitData().pipe(
             mergeMap(initData => {
                 console.log("init data = " + initData);
+                this._isQueryParamsNeeded = false;
                 fullUrl = this.setBaseUrl(initData);
                 return this.getAuthToken()
             }),
             mergeMap(authToken => {
                 let headers = this.setAuthHeader(authToken);
                 this._authorization = headers.Authorization;
-              
                 return this.http.get<any>(fullUrl, {  })
             })
         );
@@ -57,7 +59,10 @@ export class InCitesApiService extends BaseService {
     setBaseUrl(initData: InitData) : string {
         let baseUrl = super.setBaseUrl(initData);
         baseUrl = baseUrl + "inCites?";
-        baseUrl = baseUrl + this.getQueryParams();
+        if (this._isQueryParamsNeeded) {
+            baseUrl = baseUrl + this.getQueryParams();
+
+        }
         return baseUrl;
       }
 
@@ -66,7 +71,6 @@ export class InCitesApiService extends BaseService {
         urlParams = urlParams + QueryParams.Optin + "=" + this._toConnect +"&" + QueryParams.Jwt + "=" + this._authorization;
         return urlParams;
     }
-    
 }
 
 export enum QueryParams {
